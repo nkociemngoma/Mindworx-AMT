a79419de73d0d17c53c8ddbac5fa1b4c07e575e0
NathanaelCammay <86319861+NathanaelCammay@users.noreply.github.com>
2021-07-15 15:22:22 +0200

    Profile Card

---
+++ EditProfile.js
@@ -0,0 +1,20 @@
+import "./EditProfile.css";
+
+function EditProfile() {
+  return (
+    <div className="Card">
+      <div className="upper-container"></div>
+      <div className="lower-container">
+        <h1> Alisha Smith </h1>
+        <h2> Systems Development </h2>
+        <h3> Female </h3>
+        <h4> 4 Sesame Street, Soweto </h4>
+        <h5>9712124055987</h5>
+        <h6>Mother, 0845899263 </h6>
+        <button>Edit Profile</button>
+      </div>
+    </div>
+  );
+}
+
+export default EditProfile;
